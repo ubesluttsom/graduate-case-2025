104e40167b169397a3dcb8a23e7b6a06ffe92e86
Daniel Nguyen <daniel.nguyen@itera.com>
2024-08-21 16:09:52 +0200

    update

--- excursion/web/src/pages/ExcursionOverview.tsx
+++ excursion/web/src/pages/ExcursionOverview.tsx
@@ -1,9 +1,61 @@
-import headerExcursion from "../assets/img/headerExcursion.png"
+import headerExcursion from "../assets/img/headerExcursion.png";
+import dogsled from "../assets/img/dogsled.png";
+import rib from "../assets/img/rib.png";
+import whale from "../assets/img/whale.png";
+import Carousel from 'react-bootstrap/Carousel';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../style/ExcursionOverview.css'; 
 
 function ExcursionOverview() {
-    return <
-        img src={headerExcursion} alt="Test bilde2" />;
-  }
-  
-  export default ExcursionOverview;
+    return (
+        <div className="main">
+            <img src={headerExcursion} alt="Test bilde2" />
+            <div className="header">
+                <h1>Excursions</h1>
+            </div>
+            <div className="middleText">
+                <h3>Crafting Arctic Adventures</h3>
+                <p>One Thrill at a Time</p>
+                <p>Browse through our many different excursions</p>
+            </div>
+            <div>
+                <Carousel>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100"
+                            src={whale}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Whale Safari</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100"
+                            src={dogsled}
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Dog Sled</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100"
+                            src={rib}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Rib</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div>
+    );
+}
+
+export default ExcursionOverview;
