734a3aa3267f50eedad5ffbe3e95f6288e768a19
benjydp <59916344+benjydp@users.noreply.github.com>
2024-08-22 14:57:12 +0200

    Revert "Please accept our work!!"

--- excursion/api/Explore.Excursion/Services/CmsService.cs
+++ excursion/api/Explore.Excursion/Services/CmsService.cs
@@ -3,27 +3,25 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Explore.Excursion.Exceptions;
 using Explore.Excursion.Models;
-using Microsoft.AspNetCore.Http.Features.Authentication;
-using Newtonsoft.Json;
 
 namespace Explore.Excursion.Services;
 
 public class CmsService : ICmsService
 {
     private readonly HttpClient _httpClient;
-
+    
     public CmsService(HttpClient httpClient)
     {
-        _httpClient = httpClient;
+        _httpClient = httpClient;    
     }
-
+    
     public async Task<string> HealthCheckAsync()
     {
         var response = await _httpClient.GetAsync("health");
 
-        if (!response.IsSuccessStatusCode)
+        if (!response.IsSuccessStatusCode) 
             return "CMS is not healthy";
-
+        
         return await response.Content.ReadAsStringAsync();
     }
 
@@ -36,7 +34,7 @@ public class CmsService : ICmsService
     {
         return await GetAsync<GuestResponse>($"guests/{userId}");
     }
-
+    
     public async Task<RoomResponse> GetRoomByIdAsync(Guid roomId)
     {
         return await GetAsync<RoomResponse>($"rooms/{roomId}");
@@ -45,18 +43,18 @@ public class CmsService : ICmsService
     private async Task<T> GetAsync<T>(string url) where T : new()
     {
         var response = await _httpClient.GetAsync(url);
-
+        
         var statusCode = (int)response.StatusCode;
 
         if (statusCode is > 399 and < 500)
             return new T();
-
+        
         if (!response.IsSuccessStatusCode)
             throw new CmsException(await response.Content.ReadAsStringAsync());
 
         return await response.Content.ReadAsAsync<T>();
     }
-
+    
     private async Task<T> PostAsync<T>(string url, object body) where T : new()
     {
         var response = await _httpClient.PostAsJsonAsync(url, body);
@@ -64,50 +62,10 @@ public class CmsService : ICmsService
         var statusCode = (int)response.StatusCode;
         if (statusCode is > 399 and < 500)
             return new T();
-
+        
         if (!response.IsSuccessStatusCode)
             throw new CmsException(await response.Content.ReadAsStringAsync());
 
         return await response.Content.ReadAsAsync<T>();
     }
-
-    public async Task<String> GetExcursionActivity()
-    {
-
-        var excursion = new ExcursionActivity
-        {
-            Id = 1,
-            Name = "Whale safari",
-            Description = "Dette er en testaktivtet"
-        };
-
-        return JsonConvert.SerializeObject(excursion); 
-
-    }
-
-    public async Task<String> GetExcursionActivities()
-    {
-        var excursion = new ExcursionActivity
-        {
-            Id = 0,
-            Name = "WHALE SAFARI",
-            Description = "See the great whales of the artic up close along our Whale Safari."
-        };
-        var excursion1 = new ExcursionActivity
-        {
-            Id = 1,
-            Name = "DOG SLED",
-            Description = "Experience dog sledding at Svalbard with our amazing guides!"
-        };
-        var excursion3 = new ExcursionActivity
-        {
-            Id = 2,
-            Name = "RIB TRIP",
-            Description = "Experience the artic up close in this high speed adventure."
-        };
-        var excursions = new[] { excursion, excursion1, excursion3 };
-
-        return JsonConvert.SerializeObject(excursions);
-    }
-
 }
--- excursion/api/Explore.Excursion/Services/ICmsService.cs
+++ excursion/api/Explore.Excursion/Services/ICmsService.cs
@@ -7,12 +7,7 @@ namespace Explore.Excursion.Services;
 public interface ICmsService
 {
     Task<string> HealthCheckAsync();
-    
     Task<GuestResponse> CreateGuestAsync(CreateGuestRequest guest);
     Task<GuestResponse> GetGuestByIdAsync(Guid userId);
     Task<RoomResponse> GetRoomByIdAsync(Guid userId);
-
-    Task<string> GetExcursionActivity();
-
-    Task<string> GetExcursionActivities();
 }
