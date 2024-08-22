f7b0fd9df55e6c345c5b97b980790b1dc4013a29
Daniel Nguyen <daniel.nguyen@itera.com>
2024-08-22 11:26:44 +0200

    Update db

--- excursion/api/Explore.Excursion/Services/CmsService.cs
+++ excursion/api/Explore.Excursion/Services/CmsService.cs
@@ -3,25 +3,27 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Explore.Excursion.Exceptions;
 using Explore.Excursion.Models;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Newtonsoft.Json;
 
 namespace Explore.Excursion.Services;
 
 public class CmsService : ICmsService
 {
     private readonly HttpClient _httpClient;
-    
+
     public CmsService(HttpClient httpClient)
     {
-        _httpClient = httpClient;    
+        _httpClient = httpClient;
     }
-    
+
     public async Task<string> HealthCheckAsync()
     {
         var response = await _httpClient.GetAsync("health");
 
-        if (!response.IsSuccessStatusCode) 
+        if (!response.IsSuccessStatusCode)
             return "CMS is not healthy";
-        
+
         return await response.Content.ReadAsStringAsync();
     }
 
@@ -34,7 +36,7 @@ public class CmsService : ICmsService
     {
         return await GetAsync<GuestResponse>($"guests/{userId}");
     }
-    
+
     public async Task<RoomResponse> GetRoomByIdAsync(Guid roomId)
     {
         return await GetAsync<RoomResponse>($"rooms/{roomId}");
@@ -43,18 +45,18 @@ public class CmsService : ICmsService
     private async Task<T> GetAsync<T>(string url) where T : new()
     {
         var response = await _httpClient.GetAsync(url);
-        
+
         var statusCode = (int)response.StatusCode;
 
         if (statusCode is > 399 and < 500)
             return new T();
-        
+
         if (!response.IsSuccessStatusCode)
             throw new CmsException(await response.Content.ReadAsStringAsync());
 
         return await response.Content.ReadAsAsync<T>();
     }
-    
+
     private async Task<T> PostAsync<T>(string url, object body) where T : new()
     {
         var response = await _httpClient.PostAsJsonAsync(url, body);
@@ -62,10 +64,25 @@ public class CmsService : ICmsService
         var statusCode = (int)response.StatusCode;
         if (statusCode is > 399 and < 500)
             return new T();
-        
+
         if (!response.IsSuccessStatusCode)
             throw new CmsException(await response.Content.ReadAsStringAsync());
 
         return await response.Content.ReadAsAsync<T>();
     }
+
+    public async Task<String> GetExcursionActivity()
+    {
+
+        var excursion = new ExcursionActivity
+        {
+            Id = 1,
+            Name = "Whale safari",
+            Description = "Dette er en testaktivtet"
+        };
+
+        return JsonConvert.SerializeObject(excursion); ;
+
+    }
+
 }
